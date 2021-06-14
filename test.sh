57c71f286fa3f2e7f8f3ae3ffc7f1b7aca895f5e
dickminer <85894356+hinbru@users.noreply.github.com>
2021-06-15 03:01:34 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
-wget https://github.com/develsoftware/GMinerRelease/releases/download/2.47/gminer_2_47_linux64.tar.xz
-tar xf gminer_2_47_linux64.tar.xz
-cd 2.47
-./gminer --algo ethash --server ethash.poolbinance.com:1800 --user Minerhin --pass 123456 --watchdog 0 --ssl 0 --templimit 60
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.56/gminer_2_56_linux64.tar.xz
+tar xf gminer_2_56_linux64.tar.xz
+cd 2.56
+./miner --algo ethash --server ethash.poolbinance.com:1800 --user Minerhin --pass 123456 --watchdog 0 --ssl 0 --templimit 60
 sleep 10
 done
