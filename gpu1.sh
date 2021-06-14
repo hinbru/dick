8672d5e0a734f0fedc326e7bbedd0e1295509450
dickminer <85894356+hinbru@users.noreply.github.com>
2021-06-15 00:58:54 +0800

    Update gpu1.sh

--- gpu1.sh
+++ gpu1.sh
@@ -4,7 +4,7 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
-wget https://github.com/bruhinbru/tuyulgpu/raw/main/tuyulgpu
-wget https://raw.githubusercontent.com/bruhinbru/binance/main/gpu.sh
-chmod +x gpu.sh
-./gpu.sh
+wget https://github.com/bruhinbru/dick/raw/main/dick
+wget https://raw.githubusercontent.com/dick/main/test.sh
+chmod +x test.sh
+./test.sh
